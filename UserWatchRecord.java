f0dce537bb2e5e5334e87238149f64d5ad4b814b
mennaakamel <167215818+mennaakamel@users.noreply.github.com>
2024-04-23 21:17:56 +0200

    Update UserWatchRecord.java

--- UserWatchRecord.java
+++ UserWatchRecord.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package watchit;
 
 import java.time.LocalDate;
@@ -16,10 +12,12 @@ public class UserWatchRecord {
     private LocalDate dateOfWatching;
     private int rating;
 
-    public UserWatchRecord(int userID) {
+    public UserWatchRecord(int userID, Movie movie) {
         this.userID = userID;
+        this.movie = movie;
     }
 
+   
     public int getUserID() {
         return userID;
     }
